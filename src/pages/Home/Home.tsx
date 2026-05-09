b0969ee880443ab3db0c60b0302ce29621543dea
sadipkhatri21 <sadipkhatri123@gmail.com>
2026-05-09 17:41:03 +0545

    last update

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -13,20 +13,29 @@ function Home() {
     <div>
       {/* SEO Meta Tags */}
       <Helmet>
-        <title>Relax & Go Massage – Premier Spa & Massage in United Kingdom</title>
+        <title>Relax & Go Massage – Calm Body, Clear Mind</title>
         <meta
           name="description"
           content="Relax & Go Massage offers professional spa and massage treatments in London. Experience deep relaxation, stress relief & rejuvenation."
         />
-        <meta name="keywords" content="spa massage London, professional massage London, relaxation spa UK" />
+        <meta
+          name="keywords"
+          content="spa massage London, professional massage London, relaxation spa UK"
+        />
 
         {/* Open Graph (Facebook/LinkedIn) */}
-        <meta property="og:title" content="Relax & Go Massage – Premier Spa & Massage in London" />
+        <meta
+          property="og:title"
+          content="Relax & Go Massage – Calm Body, Clear Mind"
+        />
         <meta
           property="og:description"
-          content="Relax & Go Massage delivers premium spa and massage services in London for stress relief and total wellbeing."
+          content="Relax & Go Massage delivers massage services in London for stress relief and total wellbeing."
         />
-        <meta property="og:image" content="https://relaxandgomassage.co.uk/assets/Logo-e9xu4b_a.png" />
+        <meta
+          property="og:image"
+          content="https://relaxandgomassage.co.uk/assets/Logo-e9xu4b_a.png"
+        />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://relaxandgomassage.co.uk/" />
 
@@ -39,33 +48,40 @@ function Home() {
             "@context": "https://schema.org",
             "@type": "HealthAndBeautyBusiness",
             "@id": "https://relaxandgomassage.co.uk/#spa",
-            "name": "Relax & Go Massage",
-            "url": "https://relaxandgomassage.co.uk/",
-            "logo": "https://relaxandgomassage.co.uk/assets/Logo-e9xu4b_a.png",
-            "description":
+            name: "Relax & Go Massage",
+            url: "https://relaxandgomassage.co.uk/",
+            logo: "https://relaxandgomassage.co.uk/assets/Logo-e9xu4b_a.png",
+            description:
               "Professional spa and massage treatments in London focused on relaxation, stress relief and overall wellbeing.",
-            "telephone": "+44 07848930217",  
-            "address": {
+            telephone: "+44 07848930217",
+            address: {
               "@type": "PostalAddress",
-              "streetAddress": "29 Creek Road, East Molesey KT8BE",  
-              "addressLocality": "London",
-              "postalCode": "KT8 9BE",
-              "addressCountry": "GB"
+              streetAddress: "29 Creek Road, East Molesey KT8BE",
+              addressLocality: "London",
+              postalCode: "KT8 9BE",
+              addressCountry: "GB",
             },
-            "geo": {
+            geo: {
               "@type": "GeoCoordinates",
-              "latitude": 51.515,
-              "longitude": -0.144
+              latitude: 51.515,
+              longitude: -0.144,
             },
-            "openingHoursSpecification": [
+            openingHoursSpecification: [
               {
                 "@type": "OpeningHoursSpecification",
-                "dayOfWeek": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"],
-                "opens": "07:00",
-                "closes": "19:00"
-              }
+                dayOfWeek: [
+                  "Monday",
+                  "Tuesday",
+                  "Wednesday",
+                  "Thursday",
+                  "Friday",
+                  "Saturday",
+                  "Sunday",
+                ],
+                opens: "07:00",
+                closes: "19:00",
+              },
             ],
-            
           })}
         </script>
       </Helmet>
